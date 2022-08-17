04de1ec370ff95947490413ccaddf4bcf35b12bc
Frank Thomsen <frtho@sdfi.dk>
2022-08-17 17:32:40 +0200

    update test

--- test/saul-core.test.js
+++ test/saul-core.test.js
@@ -19,22 +19,27 @@ const world_elevation = 32.7199
 
 const item = await getSTAC('/collections/skraafotos2021/items/2021_83_36_4_0013_00003824', auth)
 const world_x = 587739
-const world_y = 6130952
-const image_x = 41826
-const image_y = 3204
-const world_elevation = 38.8549
+const world_y = 6130951
+const image_x = 41839.698939
+const image_y = 3204.326930
+const world_elevation = 38.874336
 
-function roundUp(num) {
-  return Number(num.toFixed(0))
-}
+function is_equalIsh(num1, num2) {
+  const deviation = 0.05
+  if (Math.abs(num1 - num2) > deviation) {
+    return false
+  } else {
+    return true
+  }
+} 
 
 // Test getZ
 try {
 
   let elevation = await getZ(world_x, world_y, auth)
 
-  assert.strictEqual(Number(elevation.toFixed(4)), world_elevation, "This is not the Z value we were hoping for.")
-
+  assert(is_equalIsh(elevation, world_elevation), "getZ fail: This is not the Z value we were hoping for.")
+  
   console.log("Test getZ OK")
 
 } catch(error) {
@@ -46,7 +51,8 @@ try {
 
   let ite = await iterate(item, image_x, image_y, auth, 0.05)
 
-  assert.strictEqual(roundUp(ite[0][0]), world_x, "The coordinate values are not equal") //test if the values are equal
+  assert(is_equalIsh(ite[0][0], world_x), "iterate fail: The coordinate values are not equal") //test if the values are equal
+  assert(is_equalIsh(ite[0][1], world_y), "iterate fail: The coordinate values are not equal") //test if the values are equal
 
   console.log("Test iterate OK")
 
@@ -59,8 +65,8 @@ try {
 
   let xy = world2image(item, world_x, world_y, world_elevation)
 
-  assert.strictEqual(roundUp(xy[0]), image_x, "The x coordinate values are not equal")
-  assert.strictEqual(roundUp(xy[1]), image_y, "The y coordinate values are not equal")
+  assert(is_equalIsh(xy[0], image_x), "world2image fail: The x coordinate values are not equal")
+  assert(is_equalIsh(xy[1], image_y), "world2image fail: The y coordinate values are not equal")
   
   console.log("Test world2image OK")
 
@@ -73,8 +79,8 @@ try {
 
   let coords = image2world(item, image_x, image_y, world_elevation)
 
-  assert.strictEqual(roundUp(coords[0]), world_x, "The longitude values are not equal")
-  assert.strictEqual(roundUp(coords[1]), world_y, "The latitude values are not equal")
+  assert(is_equalIsh(coords[0], world_x), "image2world fail: The longitude values are not equal")
+  assert(is_equalIsh(coords[1], world_y), "image2world fail: The latitude values are not equal")
 
   console.log("Test image2world OK")
 
